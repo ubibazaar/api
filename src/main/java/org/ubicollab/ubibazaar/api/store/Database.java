c4aca8f9ad41a5381b26725bfd40a80ee3366b36
Simon Stastny <stastny.simon@gmail.com>
2015-04-28 21:44:57 +0200

    having database now, using mariadb with mysql driver, c3po and jdbc

---
+++ src/main/java/org/ubicollab/ubibazaar/api/store/Database.java
@@ -0,0 +1,41 @@
+package org.ubicollab.ubibazaar.api.store;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class Database {
+
+  static ComboPooledDataSource cpds;
+
+  static {
+    try {
+      Properties prop = new Properties();
+      InputStream in = Database.class.getResourceAsStream("/api.properties");
+      prop.load(in);
+      in.close();
+      
+      
+      cpds = new ComboPooledDataSource();
+      cpds.setDriverClass("com.mysql.jdbc.Driver");
+
+      cpds.setJdbcUrl(prop.getProperty("jdbc_url"));
+      cpds.setUser(prop.getProperty("user"));
+      cpds.setPassword(prop.getProperty("password"));
+
+      cpds.setMinPoolSize(5);
+      cpds.setAcquireIncrement(5);
+      cpds.setMaxPoolSize(20);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static Connection getConnection() throws SQLException {
+    return cpds.getConnection();
+  }
+  
+}
