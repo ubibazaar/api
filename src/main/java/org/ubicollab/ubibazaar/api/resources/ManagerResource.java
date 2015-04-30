6928bad9713040ee37298af9f3df38d1b6463ae3
Simon Stastny <stastny.simon@gmail.com>
2015-04-30 19:15:58 +0200

    manager resource implementation

--- src/main/java/org/ubicollab/ubibazaar/api/resources/ManagerResource.java
+++ src/main/java/org/ubicollab/ubibazaar/api/resources/ManagerResource.java
@@ -1,16 +1,25 @@
 package org.ubicollab.ubibazaar.api.resources;
 
+import java.net.URI;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-import org.ubicollab.ubibazaar.api.store.MockStore;
+import org.ubicollab.ubibazaar.api.ServerProperties;
+import org.ubicollab.ubibazaar.api.store.ManagerStore;
+import org.ubicollab.ubibazaar.core.Manager;
 
 import com.google.gson.Gson;
 
@@ -20,31 +29,34 @@ public class ManagerResource {
   @GET
   @Path("/")
   @Produces(MediaType.APPLICATION_JSON)
-  public String getAll() {
-    return new Gson().toJson(MockStore.managers);
+  public Response getAll() {
+    return Response.ok(new Gson().toJson(ManagerStore.getAll())).build();
   }
 
   @GET
   @Path("/{id}")
   @Produces(MediaType.APPLICATION_JSON)
-  public String getById(@PathParam(value = "id") String id) {
-    return new Gson().toJson(MockStore.managers.stream()
-        .filter(manager -> manager.getId().equals(id))
-        .findAny()
-        .get());
+  public Response getById(@PathParam(value = "id") String id) {
+    Manager found = ManagerStore.getById(id);
+
+    // fail fast and return error if does not exist
+    // check for existence of the entity
+    if (found == null) {
+      return Response.status(Response.Status.NOT_FOUND).build();
+    }
+    
+    return Response.ok(new Gson().toJson(found)).build();
   }
 
   @GET
   @Path("query")
   @Produces(MediaType.APPLICATION_JSON)
-  public String getForQuery(
-      @QueryParam(value = "platform") String type,
+  public Response getForQuery(
+      @QueryParam(value = "type") String type,
       @QueryParam(value = "owner") String owner,
       @QueryParam(value = "device") String device
       ) {
-    return new Gson()
-        .toJson(MockStore.managers
-            .stream()
+    return Response.ok(new Gson().toJson(ManagerStore.getAll().stream()
             .filter(manager -> Objects.isNull(type)
                 || manager.getType().getId().equals(type))
             .filter(manager -> Objects.isNull(owner)
@@ -54,6 +66,60 @@ public class ManagerResource {
                     .filter(d -> d.getId().equals(device))
                     .collect(Collectors.toSet())
                     .size() > 0)
-            .collect(Collectors.toList()));
+            .collect(Collectors.toList()))).build();
   }
+  
+  @POST
+  @Path("/")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response create(Manager manager) {
+    // create manager
+    Manager created = ManagerStore.create(manager);
+
+    // construct URI
+    URI uri = URI.create(ServerProperties.SERVER_URL + "/resources/managers/"
+        + created.getId());
+
+    // return response with the newly created resource's uri
+    return Response.created(uri).build();
+  }
+
+  @PUT
+  @Path("/{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response update(@PathParam(value = "id") String id, Manager manager) {
+    // first make sure the id in url and in entity are the same...
+    if (manager.getId() == null) {
+      // complete the missing id in manager entity and continue
+      manager.setId(id);
+    } else if (!manager.getId().equals(id)) {
+      // different id in url and in entity
+      return Response.status(Status.BAD_REQUEST).build();
+    } 
+    
+    // fail fast and return error if does not exist
+    // check for existence of the entity
+    if (ManagerStore.getById(id) == null) {
+      return Response.status(Response.Status.NOT_FOUND).build();
+    }
+    
+    // update
+    ManagerStore.update(manager);
+    
+    return Response.ok().build();
+  }
+
+  @DELETE
+  @Path("/{id}")
+  public Response delete(@PathParam(value = "id") String id) {
+    // fail fast and return error if does not exist
+    // check for existence of the entity
+    if (ManagerStore.getById(id) == null) {
+      return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    ManagerStore.delete(id);
+    return Response.ok().build();
+  }
+  
 }
---
+++ src/main/java/org/ubicollab/ubibazaar/api/store/ManagerStore.java
@@ -0,0 +1,164 @@
+package org.ubicollab.ubibazaar.api.store;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.Set;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.ubicollab.ubibazaar.core.Device;
+import org.ubicollab.ubibazaar.core.Manager;
+import org.ubicollab.ubibazaar.core.ManagerType;
+import org.ubicollab.ubibazaar.core.User;
+
+import com.google.common.collect.Sets;
+
+@Slf4j
+public class ManagerStore {
+
+  public static Manager getById(String id) {
+    String sql = "SELECT * FROM manager WHERE id = ?";
+
+    try (Connection conn = Database.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+      ps.setString(1, id);
+      ps.execute();
+
+      try (ResultSet rs = ps.getResultSet()) {
+        if (rs.next()) {
+          String name = rs.getString("name");
+
+          ManagerType managerType = ManagerTypeStore.getById(rs.getString("manager_type_id"));
+          User owner = UserStore.getUser(rs.getString("owner_id"));
+          Set<Device> devices = findManagedDevices(id);
+
+          return new Manager(id, name, managerType, owner, devices);
+        } else {
+          return null;
+        }
+      }
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException("Database problem. See logs for details.", e);
+    }
+  }
+
+  private static Set<Device> findManagedDevices(String managerId) {
+    String sql = ""
+        + "select d.id "
+        + "from ubibazaar.device d "
+        + "join ubibazaar.manager m on m.owner_id = d.owner_id "
+        + "join ubibazaar.manager_type mt on mt.id = m.manager_type_id and mt.platform_id = d.platform_id "
+        + "left join ubibazaar.managed_device md on md.device_id = d.id and md.manager_id = m.id "
+        + "where ((md.device_id is not null) or (mt.cardinality = 'ALL')) "
+        + "and m.id = ?";
+
+    try (Connection conn = Database.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+      ps.setString(1, managerId);
+      ps.execute();
+
+      try (ResultSet rs = ps.getResultSet()) {
+        Set<Device> results = Sets.newHashSet();
+
+        while (rs.next()) {
+          String id = rs.getString("id");
+          results.add(DeviceStore.getById(id));
+        }
+
+        return results;
+      }
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException("Database problem. See logs for details.", e);
+    }
+  }
+
+  public static Collection<Manager> getAll() {
+    String sql = "SELECT * FROM manager";
+
+    try (Connection conn = Database.getConnection();
+        Statement ps = conn.createStatement()) {
+      ps.execute(sql);
+
+      try (ResultSet rs = ps.getResultSet()) {
+        Set<Manager> results = Sets.newHashSet();
+
+        while (rs.next()) {
+          String id = rs.getString("id");
+          String name = rs.getString("name");
+
+          ManagerType managerType = ManagerTypeStore.getById(rs.getString("manager_type_id"));
+          User owner = UserStore.getUser(rs.getString("owner_id"));
+
+          Set<Device> devices = findManagedDevices(id);
+
+          results.add(new Manager(id, name, managerType, owner, devices));
+        }
+
+        return results;
+      }
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException("Database problem. See logs for details.", e);
+    }
+  }
+
+  public static Manager create(Manager manager) {
+    // generate user id
+    manager.setId(IdGenerator.generateId());
+
+    String sql = "INSERT INTO manager (id, name, platform_id, manager_type_id, owner_id) "
+        + "VALUES (?,?,?,?,?)";
+
+    try (Connection conn = Database.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+      ps.setString(1, manager.getId());
+      ps.setString(2, manager.getName());
+      ps.setString(4, manager.getType().getId());
+      ps.setString(4, manager.getOwner().getId());
+      ps.execute();
+
+      return manager;
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException("Database problem. See logs for details.", e);
+    }
+  }
+
+  // FIXME should be returning?
+  public static Manager update(Manager manager) {
+    String sql = "UPDATE manager set name = ? WHERE id = ?";
+
+    try (Connection conn = Database.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+      ps.setString(1, manager.getName());
+      ps.setString(2, manager.getId());
+      ps.execute();
+
+      return manager;
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException("Database problem. See logs for details.", e);
+    }
+  }
+
+  public static void delete(String managerId) {
+    String sql = "DELETE FROM manager WHERE id = ?";
+
+    try (Connection conn = Database.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+
+      ps.setString(1, managerId);
+      ps.execute();
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException("Database problem. See logs for details.", e);
+    }
+  }
+
+}
