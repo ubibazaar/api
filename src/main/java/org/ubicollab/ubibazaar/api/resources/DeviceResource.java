d1d84975fd9c4ec30c94b7322932e2aba2155f11
Simon Stastny <stastny.simon@gmail.com>
2015-04-30 17:06:27 +0200

    device resource implemented

--- src/main/java/org/ubicollab/ubibazaar/api/resources/DeviceResource.java
+++ src/main/java/org/ubicollab/ubibazaar/api/resources/DeviceResource.java
@@ -1,16 +1,25 @@
 package org.ubicollab.ubibazaar.api.resources;
 
+import java.net.URI;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-import org.ubicollab.ubibazaar.api.store.MockStore;
+import org.ubicollab.ubibazaar.api.ServerProperties;
+import org.ubicollab.ubibazaar.api.store.DeviceStore;
+import org.ubicollab.ubibazaar.core.Device;
 
 import com.google.gson.Gson;
 
@@ -20,34 +29,92 @@ public class DeviceResource {
   @GET
   @Path("/")
   @Produces(MediaType.APPLICATION_JSON)
-  public String getAll() {
-    return new Gson().toJson(MockStore.devices);
+  public Response getAll() {
+    return Response.ok(new Gson().toJson(DeviceStore.getAll())).build();
   }
 
   @GET
   @Path("/{id}")
   @Produces(MediaType.APPLICATION_JSON)
-  public String getById(@PathParam(value = "id") String id) {
-    return new Gson().toJson(MockStore.devices.stream()
-        .filter(d -> d.getId().equals(id))
-        .findAny()
-        .get());
+  public Response getById(@PathParam(value = "id") String id) {
+    Device found = DeviceStore.getById(id);
+
+    // fail fast and return error if does not exist
+    // check for existence of the entity
+    if (found == null) {
+      return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    return Response.ok(new Gson().toJson(found)).build();
   }
 
   @GET
   @Path("query")
   @Produces(MediaType.APPLICATION_JSON)
-  public String getForQuery(
+  public Response getForQuery(
       @QueryParam(value = "platform") String platform,
       @QueryParam(value = "owner") String owner
       ) {
-    return new Gson()
-        .toJson(MockStore.devices
-            .stream()
-            .filter(d -> Objects.isNull(platform)
-                || d.getPlatform().getId().equals(platform))
-            .filter(d -> Objects.isNull(owner)
-                || d.getOwner().getId().equals(owner))
-            .collect(Collectors.toList()));
+    return Response.ok(new Gson().toJson(DeviceStore.getAll().stream()
+        .filter(a -> Objects.isNull(platform)
+            || a.getPlatform().getId().equals(platform))
+        .filter(a -> Objects.isNull(owner)
+            || a.getOwner().getId().equals(owner))
+        .collect(Collectors.toList())))
+        .build();
   }
+
+  @POST
+  @Path("/")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response create(Device device) {
+    // create device
+    Device created = DeviceStore.create(device);
+
+    // construct URI
+    URI uri = URI.create(ServerProperties.SERVER_URL + "/resources/devices/"
+        + created.getId());
+
+    // return response with the newly created resource's uri
+    return Response.created(uri).build();
+  }
+
+  @PUT
+  @Path("/{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response update(@PathParam(value = "id") String id, Device device) {
+    // first make sure the id in url and in entity are the same...
+    if (device.getId() == null) {
+      // complete the missing id in device entity and continue
+      device.setId(id);
+    } else if (!device.getId().equals(id)) {
+      // different id in url and in entity
+      return Response.status(Status.BAD_REQUEST).build();
+    }
+
+    // fail fast and return error if does not exist
+    // check for existence of the entity
+    if (DeviceStore.getById(id) == null) {
+      return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    // update
+    DeviceStore.update(device);
+
+    return Response.ok().build();
+  }
+
+  @DELETE
+  @Path("/{id}")
+  public Response delete(@PathParam(value = "id") String id) {
+    // fail fast and return error if does not exist
+    // check for existence of the entity
+    if (DeviceStore.getById(id) == null) {
+      return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    DeviceStore.delete(id);
+    return Response.ok().build();
+  }
+
 }
---
+++ src/main/java/org/ubicollab/ubibazaar/api/store/DeviceStore.java
@@ -0,0 +1,129 @@
+package org.ubicollab.ubibazaar.api.store;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.Set;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.ubicollab.ubibazaar.core.Device;
+import org.ubicollab.ubibazaar.core.Platform;
+import org.ubicollab.ubibazaar.core.User;
+
+import com.google.common.collect.Sets;
+
+@Slf4j
+public class DeviceStore {
+
+  public static Device getById(String id) {
+    String sql = "SELECT * FROM device WHERE id = ?";
+
+    try (Connection conn = Database.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+      ps.setString(1, id);
+      ps.execute();
+
+      try (ResultSet rs = ps.getResultSet()) {
+        if (rs.next()) {
+          String name = rs.getString("name");
+
+          Platform platform = PlatformStore.getPlatform(rs.getString("platform_id"));
+          User owner = UserStore.getUser(rs.getString("owner_id"));
+
+          return new Device(id, name, platform, owner);
+        } else {
+          return null;
+        }
+      }
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException("Database problem. See logs for details.", e);
+    }
+  }
+
+  public static Collection<Device> getAll() {
+    String sql = "SELECT * FROM device";
+
+    try (Connection conn = Database.getConnection();
+        Statement ps = conn.createStatement()) {
+      ps.execute(sql);
+
+      try (ResultSet rs = ps.getResultSet()) {
+        Set<Device> results = Sets.newHashSet();
+
+        while (rs.next()) {
+          String id = rs.getString("id");
+          String name = rs.getString("name");
+
+          Platform platform = PlatformStore.getPlatform(rs.getString("platform_id"));
+          User owner = UserStore.getUser(rs.getString("owner_id"));
+
+          results.add(new Device(id, name, platform, owner));
+        }
+
+        return results;
+      }
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException("Database problem. See logs for details.", e);
+    }
+  }
+
+  public static Device create(Device device) {
+    // generate user id
+    device.setId(IdGenerator.generateId());
+
+    String sql = "INSERT INTO device (id, name, platform_id, owner_id) VALUES (?,?,?,?)";
+
+    try (Connection conn = Database.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+      ps.setString(1, device.getId());
+      ps.setString(2, device.getName());
+      ps.setString(3, device.getPlatform().getId());
+      ps.setString(4, device.getOwner().getId());
+      ps.execute();
+
+      return device;
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException("Database problem. See logs for details.", e);
+    }
+  }
+
+  // FIXME should be returning?
+  public static Device update(Device device) {
+    String sql = "UPDATE device set name = ?, platform_id = ? WHERE id = ?";
+
+    try (Connection conn = Database.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+      ps.setString(1, device.getName());
+      ps.setString(2, device.getPlatform().getId());
+      ps.setString(3, device.getId());
+      ps.execute();
+
+      return device;
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException("Database problem. See logs for details.", e);
+    }
+  }
+
+  public static void delete(String deviceId) {
+    String sql = "DELETE FROM device WHERE id = ?";
+
+    try (Connection conn = Database.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+
+      ps.setString(1, deviceId);
+      ps.execute();
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException("Database problem. See logs for details.", e);
+    }
+  }
+
+}
