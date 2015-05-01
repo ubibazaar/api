98d240f1a3aedc249a5afe2f96a690d2dbffe7b9
Simon Stastny <stastny.simon@gmail.com>
2015-05-01 19:20:02 +0200

    database now using jdbc resource on application server

--- src/main/java/org/ubicollab/ubibazaar/api/store/Database.java
+++ src/main/java/org/ubicollab/ubibazaar/api/store/Database.java
@@ -5,36 +5,52 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Database {
 
-  static ComboPooledDataSource cpds;
+  public static final String DATASOURCE_CONTEXT;
 
   static {
+    String loaded = null;
+
     try {
       Properties prop = new Properties();
       InputStream in = Database.class.getResourceAsStream("/db.properties");
       prop.load(in);
       in.close();
 
-      cpds = new ComboPooledDataSource();
-      cpds.setDriverClass("com.mysql.jdbc.Driver");
-
-      cpds.setJdbcUrl(prop.getProperty("jdbc_url"));
-      cpds.setUser(prop.getProperty("user"));
-      cpds.setPassword(prop.getProperty("password"));
-
-      cpds.setMinPoolSize(5);
-      cpds.setAcquireIncrement(5);
-      cpds.setMaxPoolSize(20);
+      loaded = prop.getProperty("datasource_jndi_name");
     } catch (Exception e) {
       e.printStackTrace();
+    } finally {
+      DATASOURCE_CONTEXT = loaded;
     }
   }
 
   public static Connection getConnection() throws SQLException {
-    return cpds.getConnection();
+    Connection result = null;
+    try {
+      Context initialContext = new InitialContext();
+      DataSource datasource = (DataSource) initialContext.lookup(DATASOURCE_CONTEXT);
+      if (datasource != null) {
+        result = datasource.getConnection();
+      }
+      else {
+        log.info("Failed to lookup datasource.");
+      }
+    } catch (NamingException ex) {
+      log.info("Cannot get connection: " + ex);
+    } catch (SQLException ex) {
+      log.info("Cannot get connection: " + ex);
+    }
+    return result;
   }
 
 }
