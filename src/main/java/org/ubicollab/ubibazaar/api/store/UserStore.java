a85cdf62a2ef75107f9237cce64e9aa91c9c9377
Simon Stastny <stastny.simon@gmail.com>
2015-05-10 10:09:08 +0200

    giving user group to newly created users

--- src/main/java/org/ubicollab/ubibazaar/api/store/UserStore.java
+++ src/main/java/org/ubicollab/ubibazaar/api/store/UserStore.java
@@ -20,6 +20,13 @@ public class UserStore {
     // hash password
     user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
 
+    insertToUserTable(user);
+    insertToUserGroupTable(user);
+    
+    return user;
+  }
+
+  private static User insertToUserTable(User user) {
     String sql = "INSERT INTO user (id, name, username, password) VALUES (?,?,?,?)";
 
     try (Connection conn = Database.getConnection();
@@ -28,6 +35,22 @@ public class UserStore {
       ps.setString(2, user.getName());
       ps.setString(3, user.getUsername());
       ps.setString(4, user.getPassword()); // comes in hashed already
+      ps.execute();
+
+      return user;
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException("Database problem. See logs for details.", e);
+    }
+  }
+  
+  private static User insertToUserGroupTable(User user) {
+    String sql = "INSERT INTO user_group (group_name, user_id) VALUES (?,?)";
+
+    try (Connection conn = Database.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+      ps.setString(1, "user");
+      ps.setString(2, user.getId());
       ps.execute();
 
       return user;
